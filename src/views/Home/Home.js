e7eac2e4c7bb422a25f9d9870a8d55c775f1439b
Brendon Willian <bmota@its.jnj.com>
2018-02-07 15:15:57 -0200

    Set current position on firebase server

--- src/views/Home/Home.js
+++ src/views/Home/Home.js
@@ -1,11 +1,29 @@
 import React, { Component } from 'react';
 import { Text, View } from 'react-native';
+import firebase from 'react-native-firebase';
 
 export default class Home extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {};
+        this.watchPosition();
+    }
+    watchPosition() {
+        navigator
+            .geolocation
+            .watchPosition(currentPosition => this.setCurrentPosition(currentPosition));
+    }
+    setCurrentPosition(currentPosition) {
+        const uid = firebase.auth().currentUser.uid;
+        firebase
+            .database()
+            .ref(`users/${uid}/position`)
+            .set(currentPosition);
+    }
     render() {
         return (
             <View>
-                <Text>Test</Text>
+                <Text>Home</Text>
             </View>
         );
     }
