5009ce7bb31343de46ab55cd05590f525689ebd7
Brendon Willian <bmota@its.jnj.com>
2018-02-09 14:09:30 -0200

    Set district

--- src/views/Home/Home.js
+++ src/views/Home/Home.js
@@ -12,25 +12,27 @@ export default class Home extends Component {
     constructor(props) {
         super(props);
         this.state = {};
-        this.setCurrentPosition();
+        navigator.geolocation.watchPosition(position => this.setCurrentPosition(position));
     }
     getAddressByLatLong(latitude, longitude) {
         const { MAPS_API_KEY } = config;
         const url = `https://maps.googleapis.com/maps/api/geocode/json?latlng=${latitude},${longitude}&key=${MAPS_API_KEY}`;
         return fetch(url).then(data => data.json());
     }
-    setCurrentPosition() {
-        // const uid = firebase.auth().currentUser.uid;
-        navigator
-            .geolocation
-            .watchPosition(currentPosition => {
-                this.getAddressByLatLong(currentPosition.coords.latitude, currentPosition.coords.longitude)
-                    .then(console.log);
-                // firebase
-                //     .database()
-                //     .ref(`users/${uid}/position`)
-                //     .set(currentPosition);
-            });
+    getDistrictFromAddress(address) {
+        const results = address.results;
+        const length = results.length;
+        return results[length - 4];
+    }
+    async setCurrentPosition(position) {
+        const uid = firebase.auth().currentUser.uid;
+        const address = await this.getAddressByLatLong(position.coords.latitude, position.coords.longitude);
+        const { formatted_address } = this.getDistrictFromAddress(address);
+        position.district = formatted_address;
+        firebase
+            .database()
+            .ref(`users/${uid}/position`)
+            .set(position);
     }
     render() {
         return (
