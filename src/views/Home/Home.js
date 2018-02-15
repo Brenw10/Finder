f4c23f0e2089e78824cb86249697ecf54df05619
Brendon Willian <bmota@its.jnj.com>
2018-02-15 09:45:00 -0200

    Saving district on firebase

--- src/views/Home/Home.js
+++ src/views/Home/Home.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import { View } from 'react-native';
 import firebase from 'react-native-firebase';
 import CloserUsersList from 'Finder/src/components/CloserUsersList';
-import config from 'Finder/src/config';
+import keys from 'Finder/src/config/keys';
 
 export default class Home extends Component {
     static navigationOptions = {
@@ -16,20 +16,22 @@ export default class Home extends Component {
     componentDidMount() {
         navigator.geolocation.watchPosition(position => this.setCurrentPosition(position));
     }
-    getAddressByLatLong(latitude, longitude) {
-        const { MAPS_API_KEY } = config;
+    getAddressesByLatLong(latitude, longitude) {
+        const { MAPS_API_KEY } = keys;
         const url = `https://maps.googleapis.com/maps/api/geocode/json?latlng=${latitude},${longitude}&key=${MAPS_API_KEY}`;
-        return fetch(url).then(data => data.json());
+        return fetch(url).then(data => data.json()).then(data => data.results);
     }
-    getDistrictFromAddress(address) {
-        const results = address.results;
-        const length = results.length;
-        return results[length - 4];
+    getDistrictFromAddress(addresses) {
+        return addresses.find(address =>
+            address.types.includes('postal_code') &&
+            address.types.includes('postal_code_prefix') &&
+            address.types.length === 2
+        );
     }
     async setCurrentPosition(position) {
         const uid = firebase.auth().currentUser.uid;
-        const address = await this.getAddressByLatLong(position.coords.latitude, position.coords.longitude);
-        const { formatted_address } = this.getDistrictFromAddress(address);
+        const addresses = await this.getAddressesByLatLong(position.coords.latitude, position.coords.longitude);
+        const { formatted_address } = this.getDistrictFromAddress(addresses);
         position.district = formatted_address;
         firebase.database().ref(`users/${uid}/position`).set(position);
     }
