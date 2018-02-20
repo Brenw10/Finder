418f35b8479815f6a7a9995ba489ba3e5061e445
Brendon Willian <bmota@its.jnj.com>
2018-02-20 15:03:27 -0300

    Loader on header when position changes

--- src/views/Home/Home.js
+++ src/views/Home/Home.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, ActivityIndicator } from 'react-native';
 import firebase from 'react-native-firebase';
 import CloserUsersList from 'Finder/src/components/CloserUsersList';
 import styles from 'Finder/src/styles/Home';
@@ -7,11 +7,15 @@ import geolocation from 'Finder/src/services/geolocation';
 import Ionicons from 'react-native-vector-icons/Ionicons';
 
 export default class Home extends Component {
-    static navigationOptions = {
-        title: 'Home',
-        headerLeft: null,
-        gesturesEnabled: false,
-        tabBarIcon: ({ tintColor }) => <Ionicons name='ios-home-outline' size={25} color={tintColor} />
+    static navigationOptions = ({ navigation }) => {
+        const { params } = navigation.state;
+        return {
+            title: 'Home',
+            headerLeft: null,
+            gesturesEnabled: false,
+            headerRight: params && params.isLoading && <ActivityIndicator size="small" color="#000" style={styles.loader} />,
+            tabBarIcon: ({ tintColor }) => <Ionicons name='ios-home-outline' size={25} color={tintColor} />
+        };
     }
     constructor(props) {
         super(props);
@@ -29,11 +33,13 @@ export default class Home extends Component {
         return components.find(address => address.types.includes('sublocality_level_1'));
     }
     async setCurrentPosition(position) {
+        this.props.navigation.setParams({ isLoading: true });
         const uid = firebase.auth().currentUser.uid;
         const addresses = await geolocation.getAddressesByLatLong(position.coords.latitude, position.coords.longitude);
         const { long_name } = this.getDistrictFromAddress(addresses);
         position.district = long_name;
         await firebase.database().ref(`users/${uid}/position`).set(position);
+        this.props.navigation.setParams({ isLoading: false });
     }
     render() {
         return (
