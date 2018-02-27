ed80d1fdbd854a2bd911148cca7ad5ad49bb8e81
Brendon Willian <bmota@its.jnj.com>
2018-02-27 11:08:18 -0300

    Change launch screen ios Change login animation

--- src/views/Login/LoginOptions.js
+++ src/views/Login/LoginOptions.js
@@ -13,16 +13,16 @@ export default class LoginOptions extends Component {
     render() {
         return (
             <View style={styles.container}>
-                <View animation='zoomIn' delay={600} duration={400}>
+                <View animation='fadeInDownBig' delay={600} duration={400}>
                     <CustomButton buttonStyle={styles.registerButton}
                         text='Create an account' onPress={() => this.props.register()} />
                 </View>
-                <View style={styles.separatorContainer} animation={'zoomIn'} delay={700} duration={400}>
+                <View style={styles.separatorContainer} animation={'fadeInDownBig'} delay={700} duration={400}>
                     <View style={styles.separatorLine} />
                     <Text style={styles.separatorOr}>Or</Text>
                     <View style={styles.separatorLine} />
                 </View>
-                <View animation='zoomIn' delay={800} duration={400}>
+                <View animation='fadeInDownBig' delay={800} duration={400}>
                     <CustomButton buttonStyle={styles.loginButton}
                         text='Sign In' onPress={() => this.props.login()} />
                 </View>
