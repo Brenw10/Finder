a810d4fc83d026dd425b02f66d94149029b07efe
Brendon Willian <bmota@its.jnj.com>
2018-02-28 10:31:06 -0300

    ProfileImage component

---
+++ src/components/ProfileImage.js
@@ -0,0 +1,39 @@
+import React, { Component } from 'react';
+import { View, TouchableOpacity } from 'react-native';
+import FontAwesome from 'react-native-vector-icons/FontAwesome';
+import Spinner from 'react-native-loading-spinner-overlay';
+import firebase from 'react-native-firebase';
+import upload from 'Finder/src/services/upload';
+import ImagePicker from 'react-native-image-crop-picker';
+
+export default class ProfileImage extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            isLoading: false
+        };
+    }
+    selectImage() {
+        ImagePicker.openPicker({ width: 1000, height: 1200, cropping: true })
+            .then(image => this.setUserImage(image))
+            .catch(console.log);
+    }
+    async setUserImage(image) {
+        this.setState({ isLoading: true });
+        const currentUser = firebase.auth().currentUser;
+        const photo_url = await upload.uploadImage(`users/${currentUser.uid}`, 'profile', image);
+        await firebase.database().ref(`users/${currentUser.uid}/profile`).update({ photo_url });
+        await this.props.success();
+        this.setState({ isLoading: false });
+    }
+    render() {
+        return (
+            <View style={this.props.containerStyle}>
+                <TouchableOpacity onPress={() => this.selectImage()}>
+                    <FontAwesome name='pencil' size={23} color='black' />
+                </TouchableOpacity>
+                <Spinner visible={this.state.isLoading} />
+            </View>
+        );
+    }
+}
--- src/views/profile/Profile.js
+++ src/views/profile/Profile.js
@@ -1,13 +1,10 @@
 import React, { Component } from 'react';
 import { View, Text, TouchableOpacity, Image } from 'react-native';
-import ImagePicker from 'react-native-image-crop-picker';
 import styles from 'Finder/src/styles/Profile';
 import anonymous from 'Finder/src/images/anonymous.png';
 import auth from 'Finder/src/services/auth';
-import Spinner from 'react-native-loading-spinner-overlay';
-import upload from 'Finder/src/services/upload';
-import firebase from 'react-native-firebase';
 import FontAwesome from 'react-native-vector-icons/FontAwesome';
+import ProfileImage from 'Finder/src/components/ProfileImage';
 
 export default class Profile extends Component {
     static navigationOptions = {
@@ -16,7 +13,6 @@ export default class Profile extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            isLoading: false,
             isEditable: false
         };
     }
@@ -32,19 +28,6 @@ export default class Profile extends Component {
             return auth.getCurrentUser().then(currentUser => this.setState({ currentUser, isEditable: true }));
         }
     }
-    selectImage() {
-        ImagePicker.openPicker({ width: 1000, height: 1200, cropping: true })
-            .then(image => this.setUserImage(image))
-            .catch(console.log);
-    }
-    async setUserImage(image) {
-        this.setState({ isLoading: true });
-        const currentUser = firebase.auth().currentUser;
-        const photo_url = await upload.uploadImage(`users/${currentUser.uid}`, 'profile', image);
-        await firebase.database().ref(`users/${currentUser.uid}/profile`).update({ photo_url });
-        await this.loadUser();
-        this.setState({ isLoading: false });
-    }
     render() {
         return (
             <View style={styles.container}>
@@ -52,7 +35,6 @@ export default class Profile extends Component {
                 <TouchableOpacity onPress={() => this.props.navigation.goBack()} style={styles.backButton}>
                     <FontAwesome name='angle-left' color='white' size={35} />
                 </TouchableOpacity>
-                <Spinner visible={this.state.isLoading} />
             </View>
         );
     }
@@ -84,10 +66,6 @@ export default class Profile extends Component {
     }
     renderEditImage() {
         if (!this.state.isEditable) return;
-        return (
-            <TouchableOpacity style={styles.editAvatar} onPress={() => this.selectImage()}>
-                <FontAwesome name='pencil' size={23} color='black' />
-            </TouchableOpacity>
-        );
+        return <ProfileImage containerStyle={styles.editAvatar} success={() => this.loadUser()} />;
     }
 }
